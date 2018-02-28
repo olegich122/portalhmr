bd6f64e377c149b5f30686aae64a69a07399b92f
Mykhailo Shevchenko <mikleshef@gmail.com>
2018-02-28 22:01:07 +0200

    fixed modulation

--- ClientApp/app/portal/portal.router.module.ts
+++ ClientApp/app/portal/portal.router.module.ts
@@ -1,19 +1,26 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { CounterComponent } from './components/counter/counter.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./components/home/home.component";
+import { CounterComponent } from "./components/counter/counter.component";
+import { FetchDataComponent } from "./components/fetchdata/fetchdata.component";
+import { PortalComponent } from "./components/portal/portal.component";
 
 const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent },
-    { path: 'counter', component: CounterComponent },
-    { path: 'fetch-data', component: FetchDataComponent },
-    { path: '**', redirectTo: 'home' }
+  {
+    path: "",
+    component: PortalComponent,
+    children: [
+      { path: "", redirectTo: "home", pathMatch: "full" },
+      { path: "home", component: HomeComponent },
+      { path: "counter", component: CounterComponent },
+      { path: "fetch-data", component: FetchDataComponent },
+      { path: "**", redirectTo: "home" }
+    ]
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class PortalRoutingModule { }
+export class PortalRoutingModule {}
